9b0e4f8728c66a88ad09faf3d529df3a1b8b90c5
haoyitao <noseparte@aliyun.com>
2020-05-16 17:44:46 +0800

    add leetcode 1252. 奇数值单元格的数目

--- src/main/java/com/awaken/leetcode/array/OddCells.java
+++ src/main/java/com/awaken/leetcode/array/OddCells.java
@@ -1,4 +1,38 @@
 package com.awaken.leetcode.array;
 
+/**
+ * @Auther: Noseparte
+ * @Date: 2020/5/15 8:09 下午
+ * @Description:
+ *
+ *          <p>1252. 奇数值单元格的数目</p>
+ */
 public class OddCells {
+
+    public int oddCells(int n, int m, int[][] indices) {
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        for (int i = 0; i < indices.length; i++) {
+            row[indices[i][0]]++;
+            col[indices[i][1]]++;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if ((row[i] + col[j]) % 2 > 0)
+                    ans++;
+            }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int cells1 = new OddCells().oddCells(2, 3, new int[][]{{0, 1}, {1, 1}});
+        System.out.println(cells1);
+        int cells = new OddCells().oddCells(2, 2, new int[][]{{1, 1}, {0, 0}});
+        System.out.println(cells);
+    }
+
+
 }
--- src/main/java/com/awaken/leetcode/multithreading/FizzBuzz.java
+++ src/main/java/com/awaken/leetcode/multithreading/FizzBuzz.java
@@ -1,4 +1,77 @@
 package com.awaken.leetcode.multithreading;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.function.IntConsumer;
+
 public class FizzBuzz {
+
+    private int n;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    private static CyclicBarrier barrier = new CyclicBarrier(4);
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                printFizz.run();
+            }
+            try {
+                barrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 == 0) {
+                printBuzz.run();
+            }
+            try {
+                barrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                printFizzBuzz.run();
+            }
+            try {
+                barrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                printNumber.accept(i);
+            }
+            try {
+                barrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
